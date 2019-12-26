b7df444322729da4c84dfefad9450174ea8b3b3d
sma11case <udf.q@qq.com>
2019-12-26 09:42:15 +0800

    add nats.c/protobuf-c

---
+++ nats.c/doc/html/navtreedata.js
@@ -0,0 +1,33 @@
+var NAVTREE =
+[
+  [ "NATS C Client with Streaming support", "index.html", [
+    [ "NATS C client.", "index.html", [
+      [ "Introduction", "index.html#intro_sec", null ],
+      [ "Installation", "index.html#install_sec", null ],
+      [ "Frequently Asked Questions", "index.html#faq_sec", null ],
+      [ "Other Documentation", "index.html#other_doc_section", null ]
+    ] ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"deprecated.html",
+"group__opts_group.html#gaa1f09416d443fa7a6af6b695da563eba"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ nats.c/src/util.h
@@ -0,0 +1,120 @@
+// Copyright 2015-2019 The NATS Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef UTIL_H_
+#define UTIL_H_
+
+#include "natsp.h"
+
+#define TYPE_NOT_SET    (0)
+#define TYPE_STR        (1)
+#define TYPE_BOOL       (2)
+#define TYPE_NUM        (3)
+#define TYPE_INT        (4)
+#define TYPE_LONG       (5)
+#define TYPE_DOUBLE     (6)
+#define TYPE_ARRAY      (7)
+#define TYPE_ULONG      (8)
+
+typedef struct
+{
+    void    **values;
+    int     typ;
+    int     eltSize;
+    int     size;
+    int     cap;
+
+} nats_JSONArray;
+
+typedef struct
+{
+    char    *name;
+    int     typ;
+    union
+    {
+            char            *vstr;
+            bool            vbool;
+            long double     vdec;
+            nats_JSONArray  *varr;
+    } value;
+
+} nats_JSONField;
+
+typedef struct
+{
+    char        *str;
+    natsStrHash *fields;
+
+} nats_JSON;
+
+#define nats_IsStringEmpty(s) (((s == NULL) || (s[0] == '\0')) ? true : false)
+
+#define snprintf_truncate(d, szd, f, ...) if (snprintf((d), (szd), (f), __VA_ARGS__) >= (int) (szd)) { \
+    int offset = (int) (szd) - 2;         \
+    if (offset > 0) (d)[offset--] = '.';  \
+    if (offset > 0) (d)[offset--] = '.';  \
+    if (offset > 0) (d)[offset--] = '.';  \
+}
+
+int64_t
+nats_ParseInt64(const char *d, int dLen);
+
+natsStatus
+nats_ParseControl(natsControl *control, const char *line);
+
+natsStatus
+nats_CreateStringFromBuffer(char **newStr, natsBuffer *buf);
+
+const char*
+nats_GetBoolStr(bool value);
+
+void
+nats_NormalizeErr(char *error);
+
+natsStatus
+nats_JSONParse(nats_JSON **json, const char *str, int strLen);
+
+natsStatus
+nats_JSONGetValue(nats_JSON *json, const char *fieldName, int fieldType, void **addr);
+
+natsStatus
+nats_JSONGetArrayValue(nats_JSON *json, const char *fieldName, int fieldType, void ***array, int *arraySize);
+
+void
+nats_JSONDestroy(nats_JSON *json);
+
+void
+nats_Base32_Init(void);
+
+natsStatus
+nats_Base32_DecodeString(const char *src, char *dst, int dstMax, int *dstLen);
+
+natsStatus
+nats_Base64RawURL_EncodeString(const unsigned char *src, int srcLen, char **pDest);
+
+uint16_t
+nats_CRC16_Compute(unsigned char *data, int len);
+
+bool
+nats_CRC16_Validate(unsigned char *data, int len, uint16_t expected);
+
+natsStatus
+nats_ReadFile(natsBuffer **buffer, int initBufSize, const char *fn);
+
+bool
+nats_HostIsIP(const char *host);
+
+natsStatus
+nats_GetJWTOrSeed(char **val, const char *content, int item);
+
+#endif /* UTIL_H_ */
